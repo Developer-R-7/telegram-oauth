a2efaeacc3a07eea572c51adeb7d30e2f1110bbc
mdfarhaan <farhaanm110@gmail.com>
2024-10-08 21:27:02 +0530

    feat: add tg code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useQuery } from "@tanstack/react-query";
 import { QUERY_KEY } from "~/utils/keys";
-import { Telegram } from "./components/icons";
+// import { Telegram } from "./components/icons";
 
 export default function Home() {
   const { data: user } = useQuery({
@@ -20,18 +20,31 @@ export default function Home() {
 
   const isConnected = !!user?.telegramId;
 
+  const handleButtonClick = () => {
+    // if (!isConnected) {
+    //   window.location.href = `${process.env.NEXT_PUBLIC_TELEGRAM_OAUTH_URL}?userId=${user?.userId}`;
+    // } else {
+    //   // Optionally, you can add any logic that should happen when connected
+    // }
+    
+    // Load the Telegram widget script dynamically
+    const script = document.createElement('script');
+    script.src = "https://telegram.org/js/telegram-widget.js?22";
+    script.setAttribute('data-telegram-login', "trenchtestbot");
+    script.setAttribute('data-size', "large");
+    script.setAttribute('data-auth-url', "localhost:8080");
+    script.setAttribute('data-request-access', "write");
+    document.body.appendChild(script);
+  };
+
   return (
-    <div className="flex flex-col bg-black items-center justify-center h-screen">
-      <button
-        className="flex gap-1 items-center bg-white w-fit px-5 py-1.5 rounded-xl font-semibold text-lg"
-        onClick={() => {
-          if (!isConnected) {
-            window.location.href = `${process.env.NEXT_PUBLIC_TELEGRAM_OAUTH_URL}?userId=${user?.userId}`;
-          }
-        }}
-      >
-        <Telegram className="text-black" /> {isConnected ? "Connected" : "Connect"}
-      </button>
-    </div>
+   <button
+      className="flex gap-1 items-center bg-white w-fit px-5 py-1.5 rounded-xl font-semibold text-lg"
+      onClick={handleButtonClick}
+    >
+      <p>
+        {isConnected ? "Telegram" : "Connect to"} {user?.username}
+      </p>
+    </button>
   );
 }
