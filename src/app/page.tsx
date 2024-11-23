d86182a65e3e6bb21bdf0142977bbdcf0ca75753
Mohamed Sami <srmf2025@gmail.com>
2024-11-23 19:49:15 +0530

    feat: redirect to tele auth url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,24 +5,15 @@ import { QUERY_KEY } from "~/utils/keys";
 import { Telegram } from "~/components/icons";
 
 export default function Home() {
-  const handleButtonClick = () => {
-    const script = document.createElement("script");
-    script.src = "https://telegram.org/js/telegram-widget.js?22";
-    script.setAttribute("data-telegram-login", "mdf_v1_bot");
-    script.setAttribute("data-size", "large");
-    script.setAttribute("data-auth-url", "https://manta.trench.ag/api/v1/auth/telegram");
-    script.setAttribute("data-request-access", "write");
-    document.body.appendChild(script);
-  };
 
   return (
     <main className="flex items-center justify-center h-screen">
-      <button
+      <a
         className="flex gap-1 items-center bg-white w-fit px-5 py-1.5 rounded-xl font-semibold text-lg"
-        onClick={handleButtonClick}
+        href="https://oauth.telegram.org/auth?bot_id=7139668898&origin=https%3A%2F%2Ftelegram-oauth-sigma.vercel.app&embed=1&request_access=write&return_to=https%3A%2F%2Ftelegram-oauth-sigma.vercel.app%2F"
       >
         <Telegram /> Connect
-      </button>
+      </a>
     </main>
   );
 }
